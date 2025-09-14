7ca80701896e8de925860598d34c618d2c05aad5
Anita Tapaji <anita.tpj@gmail.com>
2025-09-14 20:47:48 +0200

    Build Neww Issue Page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import NavBar from "@/app/NavBar";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,10 +20,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <NavBar />
-      <main className="px-5">
-          {children}
-      </main>
+      <Theme>
+          <NavBar />
+          <main className="px-5">
+              {children}
+          </main>
+      </Theme>
       </body>
     </html>
   )
