08ced725d7463d98f37e007f7fdb6ac67ebce2c7
Anita Tapaji <anita.tpj@gmail.com>
2025-09-14 13:33:47 +0200

    Building the Navbar

---
+++ app/NavBar.tsx
@@ -0,0 +1,35 @@
+'use client'
+import React from 'react';
+import Link from "next/link";
+import classNames from "classnames"
+import {usePathname} from "next/navigation";
+
+const NavBar = () => {
+    const links =[
+        {label: "Dashboard", href:"/"},
+        {label: "Issues", href:"/issues"},
+    ]
+
+    const currentPath = usePathname()
+    console.log(currentPath)
+    return (
+        <nav className="flex gap-6 h-14 mb-5 px-5 border-b items-center">
+            <Link href="/">Logo</Link>
+            <ul className="flex gap-4">
+                {links.map(link =>
+                    <Link
+                        className={classNames({
+                            "text-zinc-900": link.href === currentPath,
+                            "text-zinc-500": link.href !== currentPath,
+                            " hover:text-zinc-800 transition-colors": true,
+                        })}
+                          key={link.href}
+                          href={link.href}>
+                        {link.label}</Link>
+                )}
+            </ul>
+        </nav>
+    );
+};
+
+export default NavBar;
---
+++ app/issues/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IssuesPage = () => {
+    return (
+        <div>
+            This is issues page
+        </div>
+    );
+};
+
+export default IssuesPage;
