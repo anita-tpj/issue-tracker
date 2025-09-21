3cf918eb476dbb09c63d7dfc7aca71a491e96f91
Anita Tapaji <anita.tpj@gmail.com>
2025-09-21 15:50:20 +0200

    Add Additional Skeletons

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -5,6 +5,7 @@ import {notFound} from "next/navigation";
 import {Card, Flex, Heading, Text} from "@radix-ui/themes";
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import ReactMarkdown from "react-markdown";
+import delay from "delay";
 
 interface Props {
     params: {id: string}
@@ -14,6 +15,7 @@ const IssueDetailPage = async ({params}: Props) => {
     const issue = await prisma.issue.findUnique({
         where: {id: parseInt(params.id)}
     })
+    await delay(2000)
 
     // if(typeof params.id !== 'number')
     //     notFound();
--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+import {Box} from "@radix-ui/themes";
 
 const LoadingNewIssuePage = () => {
     return (
-        <div>
-            Loading...
-        </div>
+        <Box className="max-w-xl">
+            <Skeleton/>
+            <Skeleton height="20rem"/>
+        </Box>
     );
 };
 
