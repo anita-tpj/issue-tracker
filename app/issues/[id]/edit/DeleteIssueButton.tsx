68a5ad7ec664d00de975d2ca4855c4e9afc9cec0
Anita Tapaji <anita.tpj@gmail.com>
2025-09-22 13:44:29 +0200

    Adding Confirmation Dialog Box

--- app/issues/[id]/edit/DeleteIssueButton.tsx
+++ app/issues/[id]/edit/DeleteIssueButton.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
-import {Button} from "@radix-ui/themes";
+import {AlertDialog, Button, Flex} from "@radix-ui/themes";
 import { TrashIcon } from "@radix-ui/react-icons"
 
 const DeleteIssueButton = ({issueId}: {issueId: number}) => {
     return (
-        <Button color="red"><TrashIcon />Delete issue</Button>
+        <AlertDialog.Root>
+            <AlertDialog.Trigger>
+                <Button color="red"><TrashIcon />Delete issue</Button>
+            </AlertDialog.Trigger>
+            <AlertDialog.Content>
+                <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+                <AlertDialog.Description>
+                    Are you sure you want to delete this issue? This action cannot be
+                    undone.
+                </AlertDialog.Description>
+                <Flex gap="3" mt="4" justify="end">
+                    <AlertDialog.Cancel>
+                        <Button variant="soft" color="gray">
+                            Cancel
+                        </Button>
+                    </AlertDialog.Cancel>
+                    <AlertDialog.Action>
+                        <Button variant="solid" color="red">
+                            Delete
+                        </Button>
+                    </AlertDialog.Action>
+                </Flex>
+            </AlertDialog.Content>
+        </AlertDialog.Root>
+
     );
 };
 
